afa40cc1b3c797a6359a9f496edb26a6ee7cbde3
lugvictoria <victory.senty@yandex.ru>
2023-04-05 19:21:48 +0300

    11 спринт использовать контекст CurrentUserContext в Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,34 @@
-function Card(props) {
+import React from "react";
+import CurrentUserContext from "../contexts/CurrentUserContext";
+
+function Card({card, onCardClick}) {
+  const currentUser = React.useContext(CurrentUserContext);
+  const isOwn = card.owner._id === currentUser._id;
+  const isLiked = card.likes.some(person => person._id === currentUser._id);
+  const cardLikeButtonClassName = 'card__like-button_active';
 
   function handleCardClick() {
-    props.onCardClick(props.card);
+    onCardClick(card);
   }
 
   return (
     <div className="card">
       <img
-        src={props.card.link}
-        alt={props.card.name}
+        src={card.link}
+        alt={card.name}
         className="card__image"
         onClick={handleCardClick} />
       <div className="card__group">
-        <h2 className="card__title">{props.card.name}</h2>
+        <h2 className="card__title">{card.name}</h2>
         <div className="card__like-button-group">
           <button
             type="button"
             className="card__like-button"
             aria-label="Добавить в избранное"></button>
-          <span className="card__like-count">{props.card.likes.length}</span>
+          <span className="card__like-count">{card.likes.length}</span>
         </div>
       </div>
-      <button
-        type="button"
-        className="card__delete-button"
-        aria-label="Удалить"></button>
+      {isOwn && <button className="card__delete-button"  type="button" aria-label="Удалить" />} 
     </div>
 
   )
